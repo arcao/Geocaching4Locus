4d9fafc447c2110ee7975c76d994683ddc29fa51
Arcao <arcao@arcao.com>
2013-04-04 06:32:51 +0200

    Updated minimum installed Locus version to 2.10.2.

--- src/com/arcao/geocaching4locus/constants/AppConstants.java
+++ src/com/arcao/geocaching4locus/constants/AppConstants.java
@@ -8,7 +8,7 @@ public interface AppConstants {
 	static final boolean USE_PRODUCTION_CONFIGURATION = true;
 	static final String STAGGING_CONFIGURATION = "com.arcao.geocaching.api.configuration.impl.StaggingConfiguration";
 	static final String PRODUCTION_CONFIGURATION = "com.arcao.geocaching.api.configuration.impl.ProductionConfiguration";
-  
+
 	static final String STAGGING_COMPRESSION_PROXY_SERVICE_URL = "http://wherigo-service.appspot.com/stagging_geocaching_proxy";
 	static final String COMPRESSION_PROXY_SERVICE_URL = "http://wherigo-service.appspot.com/geocaching_proxy";
 
@@ -23,15 +23,15 @@ public interface AppConstants {
 	static final Uri WEBSITE_URI = Uri.parse("http://geocaching4locus.eu/");
 	static final String DONATE_PAYPAL_URI = "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=arcao%%40arcao%%2ecom&lc=CZ&item_name=Geocaching4Locus&item_number=g4l&currency_code=%s&bn=PP%%2dDonationsBF%%3abtn_donateCC_LG%%2egif%%3aNonHosted";
 	static final Uri GEOCACHING_LIVE_URI = Uri.parse("http://www.geocaching.com/live");
-	
+
 	/** OAuth response parameter that contains error message - specific Groundspeak extension */
 	static final String OAUTH_ERROR_MESSAGE_PARAMETER = "oauth_error_message";
-	
-	static final Version LOCUS_MIN_VERSION = Version.parseVersion("2.8.7");
+
+	static final Version LOCUS_MIN_VERSION = Version.parseVersion("2.10.2");
 	static final String GOOGLE_PLAY_PREFIX = "https://play.google.com/store/apps/details?id=";
 	static final Uri ANDROIDPIT_LOCUS_FREE_LINK = Uri.parse("http://www.androidpit.com/en/android/market/apps/app/menion.android.locus/Locus-Free");
 	static final Uri ANDROIDPIT_LOCUS_PRO_LINK = Uri.parse("http://www.androidpit.com/en/android/market/apps/app/menion.android.locus.pro/Locus-Pro");
-	
+
 	/** Login dialog visible state */
 	static final String STATE_AUTHENTICATOR_ACTIVITY_VISIBLE = "AUTHENTICATOR_ACTIVITY_VISIBLE";
 }

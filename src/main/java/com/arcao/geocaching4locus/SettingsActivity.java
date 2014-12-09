9e145609e79253e1505651ad2c8b8a8e1711f38e
Arcao <arcao@arcao.com>
2014-12-09 08:06:49 +0100

    Fixed not working Filters sub-categories

--- src/main/java/com/arcao/geocaching4locus/SettingsActivity.java
+++ src/main/java/com/arcao/geocaching4locus/SettingsActivity.java
@@ -7,30 +7,10 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.view.MenuItem;
 
-import com.arcao.geocaching4locus.fragment.preference.AboutPreferenceFragment;
-import com.arcao.geocaching4locus.fragment.preference.AccountsPreferenceFragment;
-import com.arcao.geocaching4locus.fragment.preference.AdvancedPreferenceFragment;
-import com.arcao.geocaching4locus.fragment.preference.DownloadingPreferenceFragment;
-import com.arcao.geocaching4locus.fragment.preference.FilterPreferenceFragment;
 import com.arcao.geocaching4locus.fragment.preference.HeaderPreferenceFragment;
-import com.arcao.geocaching4locus.fragment.preference.LiveMapPreferenceFragment;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class SettingsActivity extends ActionBarActivity {
 	public static final String PARAM_FRAGMENT = "fragment";
-
-	private static final FragmentCache FRAGMENT_CACHE = new FragmentCache();
-	static {
-		FRAGMENT_CACHE.add(new HeaderPreferenceFragment());
-		FRAGMENT_CACHE.add(new AccountsPreferenceFragment());
-		FRAGMENT_CACHE.add(new FilterPreferenceFragment());
-		FRAGMENT_CACHE.add(new LiveMapPreferenceFragment());
-		FRAGMENT_CACHE.add(new DownloadingPreferenceFragment());
-		FRAGMENT_CACHE.add(new AdvancedPreferenceFragment());
-		FRAGMENT_CACHE.add(new AboutPreferenceFragment());
-	}
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -41,7 +21,7 @@ public class SettingsActivity extends ActionBarActivity {
 		String fragmentName = getIntent().getStringExtra(PARAM_FRAGMENT);
 		if (fragmentName != null) {
 			// Display the fragment as the main content.
-			getSupportFragmentManager().beginTransaction().replace(android.R.id.content, FRAGMENT_CACHE.get(fragmentName)).commit();
+			getSupportFragmentManager().beginTransaction().replace(android.R.id.content, Fragment.instantiate(this, fragmentName)).commit();
 			return;
 		}
 
@@ -69,17 +49,5 @@ public class SettingsActivity extends ActionBarActivity {
 
 	public static Intent createIntent(Context context, Class<?> preferenceFragment) {
 		return createIntent(context, preferenceFragment.getName());
-	}
-
-	private static class FragmentCache {
-		private Map<String, Fragment> cache = new HashMap<>();
-
-		public void add(Fragment fragment) {
-			cache.put(((Object)fragment).getClass().getName(), fragment);
-		}
-
-		public Fragment get(String key) {
-			return cache.get(key);
-		}
 	}
 }

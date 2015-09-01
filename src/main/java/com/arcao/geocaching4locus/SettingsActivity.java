8414611ef32f904683a0ec4afc95b903afea7f22
Arcao <arcao@arcao.com>
2015-09-01 23:55:59 +0200

    Fix for bad padding in header layout for Samsung phones

--- src/main/java/com/arcao/geocaching4locus/SettingsActivity.java
+++ src/main/java/com/arcao/geocaching4locus/SettingsActivity.java
@@ -3,7 +3,11 @@ package com.arcao.geocaching4locus;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.support.annotation.XmlRes;
 import android.view.MenuItem;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SettingsActivity extends AppCompatPreferenceActivity {
@@ -12,6 +16,11 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
 		super.onCreate(savedInstanceState);
 
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+		// hack / fix for Samsung phones - missing padding in header layout
+		if (!onIsMultiPane() && !getIntent().hasExtra(EXTRA_SHOW_FRAGMENT)) {
+			setPreferenceScreen(getPrefernceScreenFromHeader(R.xml.preference_header));
+		}
 	}
 
 	@Override
@@ -29,7 +38,9 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
 	 */
 	@Override
 	public void onBuildHeaders(List<Header> target) {
-		loadHeadersFromResource(R.xml.preference_header, target);
+		if (onIsMultiPane()) {
+			loadHeadersFromResource(R.xml.preference_header, target);
+		}
 	}
 
 	@Override
@@ -53,5 +64,21 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
 
 	public static Intent createIntent(Context context, Class<?> preferenceFragment) {
 		return createIntent(context, preferenceFragment.getName());
+	}
+
+	private PreferenceScreen getPrefernceScreenFromHeader(@XmlRes int headerRes) {
+		List<Header> headers = new ArrayList<>();
+		loadHeadersFromResource(headerRes, headers);
+
+		PreferenceScreen preferenceScreen = getPreferenceManager().createPreferenceScreen(this);
+		for (Header header : headers) {
+			Preference p = new Preference(this);
+			p.setTitle(header.titleRes);
+			// HACK with setFragment does not work click
+			p.setIntent(onBuildStartFragmentIntent(header.fragment, null, header.titleRes, 0));
+			preferenceScreen.addPreference(p);
+		}
+
+		return preferenceScreen;
 	}
 }

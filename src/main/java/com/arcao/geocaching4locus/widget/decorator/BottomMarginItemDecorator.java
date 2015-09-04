7d0fa91465056f8a54ee8b64a258cff967bd6a51
Arcao <arcao@arcao.com>
2015-09-04 21:00:30 +0200

    Fixed bottom margin in activities which use FAB

---
+++ src/main/java/com/arcao/geocaching4locus/widget/decorator/BottomMarginItemDecorator.java
@@ -0,0 +1,22 @@
+package com.arcao.geocaching4locus.widget.decorator;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.support.annotation.DimenRes;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class BottomMarginItemDecorator extends RecyclerView.ItemDecoration {
+	private final int bottomMargin;
+
+	public BottomMarginItemDecorator(Context context, @DimenRes int bottomMarginRes) {
+		this.bottomMargin = context.getResources().getDimensionPixelSize(bottomMarginRes);
+	}
+
+	@Override
+	public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+		// Add bottom margin to last item
+		if(parent.getChildAdapterPosition(view) == parent.getAdapter().getItemCount() - 1)
+			outRect.bottom = bottomMargin;
+	}
+}

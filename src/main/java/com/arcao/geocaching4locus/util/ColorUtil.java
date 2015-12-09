e837ca99b12dadd5f45225842a396e0dce54c408
Arcao <arcao@arcao.com>
2015-12-09 10:27:04 +0100

    Remove usage of hidden methods in AppCompat library

--- src/main/java/com/arcao/geocaching4locus/util/ColorUtil.java
+++ src/main/java/com/arcao/geocaching4locus/util/ColorUtil.java
@@ -2,11 +2,14 @@ package com.arcao.geocaching4locus.util;
 
 import android.content.Context;
 import android.content.res.ColorStateList;
-
-import static android.support.v7.internal.widget.ThemeUtils.getDisabledThemeAttrColor;
-import static android.support.v7.internal.widget.ThemeUtils.getThemeAttrColor;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.support.v4.graphics.ColorUtils;
+import android.util.TypedValue;
 
 public class ColorUtil {
+	private static final ThreadLocal<TypedValue> TL_TYPED_VALUE = new ThreadLocal<>();
+
 	private static final int[] DISABLED_STATE_SET = new int[]{-android.R.attr.state_enabled};
 	private static final int[] FOCUSED_STATE_SET = new int[]{android.R.attr.state_focused};
 	private static final int[] ACTIVATED_STATE_SET = new int[]{android.R.attr.state_activated};
@@ -14,6 +17,8 @@ public class ColorUtil {
 	private static final int[] CHECKED_STATE_SET = new int[]{android.R.attr.state_checked};
 	private static final int[] SELECTED_STATE_SET = new int[]{android.R.attr.state_selected};
 	private static final int[] EMPTY_STATE_SET = new int[0];
+
+	private static final int[] TEMP_ARRAY = new int[1];
 
 	private static ColorStateList mDefaultColorStateList = null;
 
@@ -64,4 +69,58 @@ public class ColorUtil {
 		}
 		return mDefaultColorStateList;
 	}
+
+	public static int getDisabledThemeAttrColor(Context context, int attr) {
+		final ColorStateList csl = getThemeAttrColorStateList(context, attr);
+		if (csl != null && csl.isStateful()) {
+			// If the CSL is stateful, we'll assume it has a disabled state and use it
+			return csl.getColorForState(DISABLED_STATE_SET, csl.getDefaultColor());
+		} else {
+			// Else, we'll generate the color using disabledAlpha from the theme
+
+			final TypedValue tv = getTypedValue();
+			// Now retrieve the disabledAlpha value from the theme
+			context.getTheme().resolveAttribute(android.R.attr.disabledAlpha, tv, true);
+			final float disabledAlpha = tv.getFloat();
+
+			return getThemeAttrColor(context, attr, disabledAlpha);
+		}
+	}
+
+	public static ColorStateList getThemeAttrColorStateList(Context context, int attr) {
+		TEMP_ARRAY[0] = attr;
+		TypedArray a = context.obtainStyledAttributes(null, TEMP_ARRAY);
+		try {
+			return a.getColorStateList(0);
+		} finally {
+			a.recycle();
+		}
+	}
+
+	public static int getThemeAttrColor(Context context, int attr, float alpha) {
+		final int color = getThemeAttrColor(context, attr);
+		final int originalAlpha = Color.alpha(color);
+		return ColorUtils.setAlphaComponent(color, Math.round(originalAlpha * alpha));
+	}
+
+	public static int getThemeAttrColor(Context context, int attr) {
+		TEMP_ARRAY[0] = attr;
+		TypedArray a = context.obtainStyledAttributes(null, TEMP_ARRAY);
+		try {
+			return a.getColor(0, 0);
+		} finally {
+			a.recycle();
+		}
+	}
+
+
+	private static TypedValue getTypedValue() {
+		TypedValue typedValue = TL_TYPED_VALUE.get();
+		if (typedValue == null) {
+			typedValue = new TypedValue();
+			TL_TYPED_VALUE.set(typedValue);
+		}
+		return typedValue;
+	}
+
 }

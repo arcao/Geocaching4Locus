572eea7d8e9c459a10a5a15ee9bdf58a876c5fb3
Arcao <arcao@arcao.com>
2016-03-30 20:57:47 +0200

    Improvements in basic membership dialog warning

--- src/main/java/com/arcao/geocaching4locus/fragment/dialog/BasicMembershipWarningDialogFragment.java
+++ src/main/java/com/arcao/geocaching4locus/fragment/dialog/BasicMembershipWarningDialogFragment.java
@@ -1,6 +1,11 @@
 package com.arcao.geocaching4locus.fragment.dialog;
 
+import android.support.annotation.NonNull;
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.arcao.geocaching4locus.R;
+import com.arcao.geocaching4locus.constants.AppConstants;
+import com.arcao.geocaching4locus.util.IntentUtil;
 
 public class BasicMembershipWarningDialogFragment extends AbstractErrorDialogFragment {
   public static final String FRAGMENT_TAG = BasicMembershipWarningDialogFragment.class.getName();
@@ -15,5 +20,22 @@ public class BasicMembershipWarningDialogFragment extends AbstractErrorDialogFra
   protected void onPositiveButtonClick() {
     super.onPositiveButtonClick();
     getActivity().finish();
+    dismiss();
+  }
+
+  @Override
+  protected void onDialogBuild(MaterialDialog.Builder builder) {
+    super.onDialogBuild(builder);
+
+    // disable auto dismiss
+    builder.autoDismiss(false);
+
+    builder.neutralText(R.string.basic_member_warning_button_manual)
+        .onNeutral(new MaterialDialog.SingleButtonCallback() {
+          @Override
+          public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+            IntentUtil.showWebPage(getActivity(), AppConstants.MANUAL_URI);
+          }
+        });
   }
 }

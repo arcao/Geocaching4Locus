13ecb49e01144bc093c12ed855daabdef35c906c
Arcao <arcao@arcao.com>
2016-09-17 06:07:28 +0200

    Added AutoValue-Parcel TypeAdapter for Coordinates class

--- src/main/java/com/arcao/geocaching4locus/authentication/util/Account.java
+++ src/main/java/com/arcao/geocaching4locus/authentication/util/Account.java
@@ -2,29 +2,38 @@ package com.arcao.geocaching4locus.authentication.util;
 
 import android.os.Parcelable;
 import android.support.annotation.Nullable;
-
+import com.arcao.auto.value.parcel.CoordinatesTypeAdapter;
 import com.arcao.geocaching.api.data.coordinates.Coordinates;
 import com.google.auto.value.AutoValue;
+import com.ryanharter.auto.value.parcel.ParcelAdapter;
 
 @AutoValue
 public abstract class Account implements Parcelable {
-    public abstract String name();
-    public abstract boolean premium();
-    @Nullable
-    public abstract String avatarUrl();
-    @Nullable
-    public abstract Coordinates homeCoordinates();
+  public abstract String name();
 
-    public static Builder builder() {
-        return new AutoValue_Account.Builder();
-    }
+  public abstract boolean premium();
 
-    @AutoValue.Builder
-    public abstract static class Builder {
-        public abstract Builder name(String name);
-        public abstract Builder premium(boolean premium);
-        public abstract Builder avatarUrl(@Nullable String avatarUrl);
-        public abstract Builder homeCoordinates(@Nullable Coordinates homeCoordinates);
-        public abstract Account build();
-    }
+  @Nullable
+  public abstract String avatarUrl();
+
+  @Nullable
+  @ParcelAdapter(CoordinatesTypeAdapter.class)
+  public abstract Coordinates homeCoordinates();
+
+  public static Builder builder() {
+    return new AutoValue_Account.Builder();
+  }
+
+  @AutoValue.Builder
+  public abstract static class Builder {
+    public abstract Builder name(String name);
+
+    public abstract Builder premium(boolean premium);
+
+    public abstract Builder avatarUrl(@Nullable String avatarUrl);
+
+    public abstract Builder homeCoordinates(@Nullable Coordinates homeCoordinates);
+
+    public abstract Account build();
+  }
 }

5731ee20e5e45b1ef8f8baf964203699da197b69
Arcao <arcao@arcao.com>
2017-10-22 05:30:21 +0200

    Removed setting alpha on disabled state (alpha is already set via state list)

--- app/src/main/java/com/arcao/geocaching4locus/dashboard/widget/DashboardButton.java
+++ app/src/main/java/com/arcao/geocaching4locus/dashboard/widget/DashboardButton.java
@@ -76,11 +76,4 @@ public class DashboardButton extends ToggleButton {
 
 		super.setChecked(checked);
 	}
-
-	@Override
-	public void setEnabled(boolean enabled) {
-		super.setEnabled(enabled);
-
-		setAlpha(enabled ? ALPHA_ENABLED : ALPHA_DISABLED);
-	}
 }

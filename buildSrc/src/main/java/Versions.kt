0fa14a2318dc97acdff21aa1a1d53d8d503b4236
Arcao <arcao@arcao.com>
2018-02-18 12:19:31 +0100

    Better dependency management and other minor Gradle build script improvements

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,28 @@
+object Versions {
+    const val min_sdk = 16
+    const val target_sdk = 27
+    const val compile_sdk = target_sdk;
+
+    const val android_gradle_plugin = "3.1.0-beta3"
+    const val gradle_versions_plugin = "0.17.0"
+    const val google_services_plugin = "3.2.0"
+    const val fabric_io_plugin = "1.25.1"
+
+    const val support = "27.0.2"
+    const val constraint_layout = "1.1.0-beta5"
+    const val geocaching_api = "2.0.2"
+    const val locus_api_android = "0.2.19"
+    const val slf4j_timber = "3.0"
+    const val timber = "4.6.1"
+    const val commons_lang3 = "3.7"
+    const val commons_collections4 = "4.1"
+    const val commons_io = "2.6"
+    const val scribejava = "5.2.0-java7again"
+    const val material_dialogs = "0.9.6.0"
+    const val firebase = "11.8.0"
+    const val crashlytics = "2.9.0"
+    const val okhttp = "3.9.1"
+    const val butterknife = "8.8.1"
+    const val auto_value = "1.5.3"
+    const val auto_value_parcel = "0.2.6"
+}

9270309992a02fa3570d537405d524434c62089d
Arcao <arcao@arcao.com>
2018-03-30 07:24:56 +0200

    Updated to latest dependencies

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -5,7 +5,7 @@ object Versions {
 
     const val android_gradle_plugin = "3.1.0"
     const val gradle_versions_plugin = "0.17.0"
-    const val google_services_plugin = "3.2.0"
+    const val google_services_plugin = "3.2.1"
     const val fabric_io_plugin = "1.25.2"
 
     const val support = "27.1.0"
@@ -13,16 +13,16 @@ object Versions {
     const val geocaching_api = "2.0.2"
     const val locus_api_android = "0.2.20"
     const val slf4j_timber = "3.0"
-    const val timber = "4.6.1"
+    const val timber = "4.7.0"
     const val commons_lang3 = "3.7"
     const val commons_collections4 = "4.1"
     const val commons_io = "2.6"
     const val scribejava = "5.3.0"
     const val material_dialogs = "0.9.6.0"
-    const val firebase = "12.0.0"
+    const val firebase = "12.0.1"
     const val crashlytics = "2.9.1"
     const val okhttp = "3.10.0"
     const val butterknife = "8.8.1"
-    const val auto_value = "1.5.3"
+    const val auto_value = "1.6"
     const val auto_value_parcel = "0.2.6"
 }

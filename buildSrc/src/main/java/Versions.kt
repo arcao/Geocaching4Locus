1fa70fef6791dce1669f4b8b53331f6ade1b54d6
Arcao <arcao@arcao.com>
2019-02-18 00:42:44 +0100

    Updated to latest dependencies

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -3,11 +3,11 @@ object Versions {
     const val target_sdk = 28
     const val compile_sdk = target_sdk
 
-    const val kotlin = "1.3.20"
-    const val kotlin_stdlib = "1.3.20"
+    const val kotlin = "1.3.21"
+    const val kotlin_stdlib = "1.3.21"
     const val kotlin_coroutines = "1.1.1"
 
-    const val android_gradle_plugin = "3.3.0"
+    const val android_gradle_plugin = "3.3.1"
     const val gradle_versions_plugin = "0.20.0"
     const val google_services_plugin = "4.2.0"
     const val fabric_io_plugin = "1.27.0"
@@ -19,22 +19,22 @@ object Versions {
     const val constraint_layout = "1.1.3"
     const val koin = "1.0.2"
     const val geocaching_api = "2.1.0"
-    const val locus_api_android = "0.3.4"
+    const val locus_api_android = "0.3.5"
     const val slf4j_timber = "3.1"
     const val timber = "4.7.1"
     const val commons_lang3 = "3.8.1"
     const val commons_collections4 = "4.2"
-    const val scribejava = "6.2.0"
+    const val scribejava = "6.3.0"
     const val material_dialogs = "0.9.6.0"
     const val firebase = "16.0.6"
-    const val crashlytics = "2.9.8"
-    const val okhttp = "3.12.1"
-    const val butterknife = "10.0.0"
+    const val crashlytics = "2.9.9"
+    const val okhttp = "3.12.1" // keep 3.12.x, 3.13.x require Android 5+ (API 21)
+    const val butterknife = "10.1.0"
     const val auto_value = "1.6.3"
     const val auto_value_parcel = "0.2.6"
     const val androidx_core_ktx = "1.0.1"
     const val androidx_ktx = "1.0.0"
     const val androidx_ktx_lifecycle = "2.0.0"
 
-    const val ktlint = "0.29.0"
+    const val ktlint = "0.30.0"
 }

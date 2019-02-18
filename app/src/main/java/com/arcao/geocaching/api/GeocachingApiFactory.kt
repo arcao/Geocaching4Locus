955fed15ca9be7b73879e8478e5c9f958223c1dd
Arcao <arcao@arcao.com>
2019-02-18 21:53:53 +0100

    Remove unused methods

--- app/src/main/java/com/arcao/geocaching/api/GeocachingApiFactory.kt
+++ app/src/main/java/com/arcao/geocaching/api/GeocachingApiFactory.kt
@@ -1,19 +1,10 @@
 package com.arcao.geocaching.api
 
-import com.arcao.geocaching.api.downloader.Downloader
-import okhttp3.OkHttpClient
 import org.koin.standalone.KoinComponent
 import org.koin.standalone.get
-import org.koin.standalone.inject
 
 @Deprecated("Use Koin")
 object GeocachingApiFactory : KoinComponent {
-    @JvmStatic
-    val okHttpClient by inject<OkHttpClient>()
-
-    @JvmStatic
-    val downloader by inject<Downloader>()
-
     @JvmStatic
     fun create() = get<GeocachingApi>()
 }

dd5ed001a5fd8fbae858a9bfb79a3c0796c94888
Arcao <arcao@arcao.com>
2019-05-08 18:54:09 +0200

    Fix when onLocationChanged is called more than twice

--- app/src/main/java/com/arcao/geocaching4locus/base/usecase/GetGpsLocationUseCase.kt
+++ app/src/main/java/com/arcao/geocaching4locus/base/usecase/GetGpsLocationUseCase.kt
@@ -9,6 +9,7 @@ import android.os.Bundle
 import androidx.core.content.getSystemService
 import com.arcao.geocaching4locus.base.coroutine.CoroutinesDispatcherProvider
 import com.arcao.geocaching4locus.base.util.hasGpsLocationPermission
+import kotlinx.coroutines.CancellableContinuation
 import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlinx.coroutines.withContext
 import timber.log.Timber
@@ -21,42 +22,53 @@ class GetGpsLocationUseCase(
     private val locationManager = requireNotNull(context.getSystemService<LocationManager>())
 
     @SuppressLint("MissingPermission")
-    suspend operator fun invoke() = withContext(dispatcherProvider.computation) {
+    suspend operator fun invoke() = withContext(dispatcherProvider.main) {
         suspendCancellableCoroutine<Location?> { result ->
             if (!context.hasGpsLocationPermission || !locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
                 result.resume(null)
                 return@suspendCancellableCoroutine
             }
 
-            val listener = object : LocationListener {
-                override fun onLocationChanged(location: Location) {
-                    locationManager.removeUpdates(this)
-                    Timber.i("Location listener removed.")
-                    result.resume(location)
-                }
+            val listener = GpsLocationListener(result, locationManager)
 
-                override fun onStatusChanged(provider: String, status: Int, extras: Bundle?) {
-                }
-
-                override fun onProviderEnabled(provider: String?) {
-                }
-
-                override fun onProviderDisabled(provider: String) {
-                    Timber.i("onProviderDisabled: $provider")
-
-                    locationManager.removeUpdates(this)
-                    Timber.i("Location listener removed.")
-                    result.resume(null)
-                }
+            result.invokeOnCancellation {
+                Timber.i("removeUpdates")
+                locationManager.removeUpdates(listener)
+                Timber.i("Location listener removed.")
             }
 
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, listener)
             Timber.i("Location listener added.")
+        }
+    }
 
-            result.invokeOnCancellation {
-                locationManager.removeUpdates(listener)
-                Timber.i("Location listener removed.")
-            }
+    private class GpsLocationListener(
+        private val result: CancellableContinuation<Location?>,
+        private val locationManager: LocationManager
+    ) : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            if (result.isCompleted) return
+
+            Timber.i("onLocationChanged")
+            locationManager.removeUpdates(this)
+            Timber.i("Location listener removed.")
+            result.resume(location)
+        }
+
+        override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+        }
+
+        override fun onProviderEnabled(provider: String?) {
+        }
+
+        override fun onProviderDisabled(provider: String?) {
+            if (result.isCompleted) return
+
+            Timber.i("onProviderDisabled: $provider")
+
+            locationManager.removeUpdates(this)
+            Timber.i("Location listener removed.")
+            result.resume(null)
         }
     }
 }

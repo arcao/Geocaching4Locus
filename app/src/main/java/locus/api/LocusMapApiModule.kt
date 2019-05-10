3a5e1230df34b6300e58c1eeccae234fa6b3b633
Arcao <arcao@arcao.com>
2019-05-10 04:45:42 +0200

    Fix missing dependency declaration

--- app/src/main/java/locus/api/LocusMapApiModule.kt
+++ app/src/main/java/locus/api/LocusMapApiModule.kt
@@ -1,12 +1,7 @@
 package locus.api
 
 import locus.api.manager.LocusMapManager
-import locus.api.mapper.DataMapper
-import locus.api.mapper.GeocacheConverter
-import locus.api.mapper.GeocacheLogConverter
-import locus.api.mapper.ImageDataConverter
-import locus.api.mapper.TrackableConverter
-import locus.api.mapper.WaypointConverter
+import locus.api.mapper.*
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
@@ -15,10 +10,11 @@ import org.koin.dsl.module
  */
 internal val locusMapApiModule = module {
     single { LocusMapManager(androidContext()) }
+    single { DataMapper(get(), get(), get()) }
+    single { GeocacheConverter(androidContext(), get(), get(), get(), get(), get()) }
+    single { GeocacheLogConverter(get()) }
+    single { ImageDataConverter() }
+    single { PointMerger(get()) }
     single { TrackableConverter() }
     single { WaypointConverter() }
-    single { ImageDataConverter() }
-    single { GeocacheLogConverter(get()) }
-    single { GeocacheConverter(androidContext(), get(), get(), get(), get(), get()) }
-    single { DataMapper(get(), get(), get()) }
 }
